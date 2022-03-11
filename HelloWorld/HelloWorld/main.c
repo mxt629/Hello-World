2a53649da04eb6ffb544c502c6a82e55c71b8068
mxt629 <xuantien.06021990@gmail.com>
2022-03-11 17:28:37 +0700

    First commit
    test commit content

---
+++ HelloWorld/HelloWorld/main.c
@@ -0,0 +1,19 @@
+//
+//  main.c
+//  HelloWorld
+//
+//  Created by Little Chick on 11/03/2022.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    
+    printf("Thao thức canh khuya với nỗi buồn \n");
+    printf("Nhớ người yêu cũ lệ trào tuôn \n");
+    printf("Người đi giết con tim vỡ \n");
+    printf("Kẻ ở đau buồn khóc tình xuông \n");
+
+    
+    return 0;
+}
